b97ff3d8e2429db4006845291c9bfaeae56b14a4
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2021-02-15 19:59:02 +0200

    Add files via upload

--- SVMMain.py
+++ SVMMain.py
@@ -1,5 +1,6 @@
 import SVMHelperMethods
 import pandas
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
@@ -25,8 +26,92 @@ supportVectorClassifier.fit(trainingSampleList, qualityTrainingSample)
 # Predict the target property values of the test sample set
 wineQualityPrediction = supportVectorClassifier.predict(testSample)
 
+# Plot the values
+plt.plot(range(0, 401), wineQualityPrediction)
+
+# Set the x label
+plt.xlabel("Elements")
+
+# Set the y label
+plt.ylabel("Prediction")
+
+# Save the plot
+plt.savefig("SVC.png")
+
+# Clear the plot
+plt.clf()
+
 # Get the length of the first one third slice of the list
 editedTrainingSampleLength = round(len(trainingSampleList) / 3)
 
 # Predict the wine quality, without using the pH column
-wineQualityPrediction = RemovePHColumn(trainingSampleList, qualityTrainingSample, testSample, supportVectorClassifier)
+wineQualityPrediction = SVMHelperMethods.RemovePHColumn(trainingSampleList, qualityTrainingSample, testSample, supportVectorClassifier)
+
+# Plot the values
+plt.plot(range(0, 401), wineQualityPrediction)
+
+# Set the x label
+plt.xlabel("Elements")
+
+# Set the y label
+plt.ylabel("Prediction")
+
+# Save the plot
+plt.savefig("RemovePHSVC.png")
+
+# Clear the plot
+plt.clf()
+
+# Predict the wine quality, by using the average pH for the pH column
+wineQualityPrediction = SVMHelperMethods.AveragePHColumn(trainingSampleList, qualityTrainingSample, testSample, supportVectorClassifier, editedTrainingSampleLength)
+
+# Plot the values
+plt.plot(range(0, 401), wineQualityPrediction)
+
+# Set the x label
+plt.xlabel("Elements")
+
+# Set the y label
+plt.ylabel("Prediction")
+
+# Save the plot
+plt.savefig("AveragePHSVC.png")
+
+# Clear the plot
+plt.clf()
+
+# Predict the wine quality, by using Logistic Regression to predict the pH column
+wineQualityPrediction = SVMHelperMethods.LogisticRegressionPHColumn(trainingSampleList, qualityTrainingSample, testSample, supportVectorClassifier, editedTrainingSampleLength)
+
+# Plot the values
+plt.plot(range(0, 401), wineQualityPrediction)
+
+# Set the x label
+plt.xlabel("Elements")
+
+# Set the y label
+plt.ylabel("Prediction")
+
+# Save the plot
+plt.savefig("LogisticRegressionSVC.png")
+
+# Clear the plot
+plt.clf()
+
+# Predict the wine quality, by using KMeans clustering to predict the pH column
+wineQualityPrediction = SVMHelperMethods.KMeansPHColumn(trainingSampleList, qualityTrainingSample, testSample, supportVectorClassifier, editedTrainingSampleLength)
+
+# Plot the values
+plt.plot(range(0, 401), wineQualityPrediction)
+
+# Set the x label
+plt.xlabel("Elements")
+
+# Set the y label
+plt.ylabel("Prediction")
+
+# Save the plot
+plt.savefig("KmeansPHPredictionSVC.png")
+
+# Clear the plot
+plt.clf()
